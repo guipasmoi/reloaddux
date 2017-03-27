47799223a62ed110f03159777097b02619c77a47
guipasmoi <guipasmoi@hotmail.com>
2017-03-27 21:15:57 +0200

    add tests reducer manager

--- tests/ReducerManager.js
+++ tests/ReducerManager.js
@@ -1,5 +1,104 @@
-describe("combineReducerTree", () => {
-  it("It can init state with init action", () => {
-    // const toto = "";
+import ReducerManager from "../src/ReducerManager";
+
+const counter1 = (state = 11, action) => {
+  return state + 1;
+};
+const counter2 = (state = 22, action) => {
+  return state + 2;
+};
+const counter3 = (state = 33, action) => {
+  return state + 3;
+};
+
+const reducersTree = {
+  AwesomeStateForCounter1: {
+    actions: ["ACTION1"],
+    reducer: counter1
+  }
+};
+
+const reducersTree1 = {
+  counters: {
+    AwesomeStateForCounter1: {
+      actions: ["ACTION1"],
+      reducer: counter1
+    }
+  }
+};
+
+const reducersTree2 = {
+  counters: {
+    AwesomeStateForCounter2: {
+      reducer: counter2
+    }
+  }
+};
+
+const reducersTree3 = {
+  counters: {
+    Awesome: {
+      StateFor: {
+        Counter3: {
+          actions: ["ACTION3"],
+          reducer: counter3
+        }
+      }
+    }
+  }
+};
+
+describe("ReducerManager", () => {
+  it("reducer do nothing if nothing is registered ", () => {
+    const reducerManager = new ReducerManager();
+    let initState;
+    let state;
+    initState = 1234;
+    state = reducerManager.reducer(initState, { type: "toto" });
+    expect(state).toBe(initState);
+    initState = { a: { b: "a value" } };
+    state = reducerManager.reducer(initState, { type: "toto" });
+    expect(state).toBe(initState);
+    initState = undefined;
+    state = reducerManager.reducer(initState, { type: "toto" });
+    expect(state).toBe(initState);
+  });
+
+  it("it can register a simple tree reducers", () => {
+    const reducerManager = new ReducerManager();
+    let initState = {};
+    let state;
+    reducerManager.addReducersTree(reducersTree);
+    state = reducerManager.reducer(initState, { type: "ACTION1" });
+    expect(state.AwesomeStateForCounter1).toBe(12);
+    state = reducerManager.reducer(state, { type: "ACTION1" });
+    expect(state.AwesomeStateForCounter1).toBe(13);
+  });
+
+  it("it can register multiple tree reducers", () => {
+    const reducerManager = new ReducerManager();
+    const initState = undefined;
+    let state;
+    reducerManager.addReducersTree(reducersTree1);
+    reducerManager.addReducersTree(reducersTree2);
+    reducerManager.addReducersTree(reducersTree3);
+    state = reducerManager.reducer(initState, { type: "ACTION1" });
+    expect(state).toMatchSnapshot();
+    state = reducerManager.reducer(state, { type: "ACTION X" });
+    expect(state).toMatchSnapshot();
+    state = reducerManager.reducer(state, { type: "ACTION3" });
+    expect(state).toMatchSnapshot();
+  });
+
+  it("it can unregister tree reducers", () => {
+    const reducerManager = new ReducerManager();
+    let initState = {};
+    let state;
+
+    reducerManager.addReducersTree(reducersTree1, reducersTree2, reducersTree3);
+    state = reducerManager.reducer(initState, { type: "ACTION1" });
+    expect(state).toMatchSnapshot();
+    reducerManager.removeReducersTree(reducersTree1);
+    state = reducerManager.reducer(state, { type: "ACTION1" });
+    expect(state).toMatchSnapshot("should not have change after action 1");
   });
 });
